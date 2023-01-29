484852c36a5fc90b7bad65bfe35296a9efc9b78d
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-01-28 22:26:16 -0600

    Earth and moon

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -1,21 +1,19 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import * as THREE from '../../libs/three/three.module.js'
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js'
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-    
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        
-    }
-    
-	render( ) {   
-        
-    }
+class App {
+	constructor() {
+		console.log('Lecture 2-1')
+
+		const container = document.createElement('div')
+		document.body.appendChild(container)
+
+		window.addEventListener('resize', this.resize.bind(this))
+	}
+
+	resize() {}
+
+	render() {}
 }
 
-export { App };
+export { App }
--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -1,22 +1,62 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import * as THREE from '../../libs/three/three.module.js'
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js'
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-    
-        
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        
-    }
-    
-	render( ) {  
-        
-    }
+class App {
+	constructor() {
+		const container = document.createElement('div')
+		document.body.appendChild(container)
+
+		// Perspective camera
+		// Distance objects appear smaller
+		this.camera = new THREE.PerspectiveCamera(
+			60,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			100
+		)
+		this.camera.position.set(0, 0, 4)
+
+		// Scene
+		this.scene = new THREE.Scene()
+		this.scene.background = new THREE.Color(0xaaaaaa)
+
+		// Lighting
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3)
+		this.scene.add(ambient)
+		const light = new THREE.DirectionalLight()
+		light.position.set(0.2, 1, 1)
+		this.scene.add(light)
+
+		// Renderer
+		this.renderer = new THREE.WebGLRenderer({ antialias: true })
+		this.renderer.setPixelRatio(window.devicePixelRatio)
+		this.renderer.setSize(window.innerWidth, window.innerHeight)
+		container.appendChild(this.renderer.domElement)
+
+		this.renderer.setAnimationLoop(this.render.bind(this))
+
+		// Add an object to the scene
+		const geometry = new THREE.BoxBufferGeometry() // Box
+		const material = new THREE.MeshStandardMaterial({ color: 0xff0000 })
+		this.mesh = new THREE.Mesh(geometry, material)
+
+		this.scene.add(this.mesh)
+
+		const controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+		window.addEventListener('resize', this.resize.bind(this))
+	}
+
+	resize() {
+		this.camera.aspect = window.innerWidth / window.innerHeight
+		this.camera.updateProjectionMatrix()
+		this.renderer.setSize(window.innerWidth, window.innerHeight)
+	}
+
+	render() {
+		this.mesh.rotateY(0.01)
+		this.renderer.render(this.scene, this.camera)
+	}
 }
 
-export { App };
+export { App }
